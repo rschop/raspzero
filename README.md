ac1ab3150c134cb5447f3aa23001c1b322dc1de7
Richard Schop <rschop@users.noreply.github.com>
2023-11-04 18:57:55 +0100

    rel 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Raspzero reposotory
+# Raspzero repository
 
 Om het script uitvoerbaar te maken:
 chmod +x <scriptnaam>
--- install_software.sh
+++ install_software.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+sudo apt-get install nano git zip
+
+git config --global user.name "Richard Schop" && git config --global user.email "raschop@outlook.com"
